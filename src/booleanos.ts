753d63907bc35787f6013d0dfebd12570362210d
anton <ramirezfonseca.la@gmail.com>
2020-06-16 16:19:02 -0500

    finazando booleanos.ts

--- src/booleanos.ts
+++ src/booleanos.ts
@@ -3,3 +3,21 @@
 let isSuperman:boolean=true; //forma 1
 let isBatman:boolean;   //forma 2
 let isAcuaman=true;     //forma 3
+
+if(isSuperman){
+    console.log("Estamos Salvados!!")
+}else{
+    console.log("Problemas!!");
+}
+
+isSuperman=convertirClark();
+
+if(isSuperman){
+    console.log("Estamos Salvados!!")
+}else{
+    console.log("Problemas!!");
+}
+
+function convertirClark(){
+    return false;
+}
