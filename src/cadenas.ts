1e971043ba710a4826e4e86caba413cf89711813
anton <ramirezfonseca.la@gmail.com>
2020-06-16 16:43:06 -0500

    se agrego cadenas.ts

---
+++ src/cadenas.ts
@@ -0,0 +1,11 @@
+let batman:string="batman";
+let linternaVerde:string="Linterna Verde"
+let volcanNegro:string="VolcanNegro";
+
+//imprime por separado
+console.log(batman);
+console.log(linternaVerde);
+console.log(volcanNegro);
+
+//imprime en una lineas
+console.log(`Heroes: ${batman},${linternaVerde},${volcanNegro}`);
