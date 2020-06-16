65f66ff98f8eab89f96dd4e5496f817d582324d1
anton <ramirezfonseca.la@gmail.com>
2020-06-16 16:54:17 -0500

    se agrego el Any.ts

---
+++ src/Any.ts
@@ -0,0 +1,20 @@
+//esta declaración es la misma que la del 
+//bloque que se encuentra comentado
+let vengador;
+let existe;
+let derrotas;
+
+/*  let vengador:undefined;
+    let existe:undefined;
+    let derrotas:undefined;*/
+
+vengador = "Dr.Strange";
+console.log(vengador.charAt(0));
+
+vengador = 2.43344;
+console.log(vengador.toFixed(2));
+
+vengador = true;
+console.log(vengador);
+
+console.log(existe);    //undefined
