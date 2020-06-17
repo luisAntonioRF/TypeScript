1746ba35bb20bad5cda226347051c0e44e84cc83
anton <ramirezfonseca.la@gmail.com>
2020-06-16 20:48:36 -0500

    se creo archivo Tupla.ts

---
+++ src/Tupla.ts
@@ -0,0 +1,10 @@
+/**
+ * las Tuplas es un nuevo tipo de Dato en TypeScript
+ * se conforman por diferentes tipos de datos y
+ * como resulrado nos da una tupla
+ */
+
+ let nombre:String="Luis Antonio";
+ let edad:number=30;
+
+ let persona:[String,number] =[nombre,edad]; 
