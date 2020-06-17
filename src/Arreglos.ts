7db9af74af08a7bb35cd89ad0ecaf4788d53f227
anton <ramirezfonseca.la@gmail.com>
2020-06-16 20:35:52 -0500

    se agrego Arreglos.ts y modificacion numeros.ts

---
+++ src/Arreglos.ts
@@ -0,0 +1,10 @@
+let arreglo:number[] = [1,2,3,4];
+
+let villanos:String[] = ["Dr.Doom","Dormamo","Duende Verde"];
+console.log(villanos[2]);
+
+//todos los villanos forma #1
+for (let index = 0; index < villanos.length; index++) {
+        console.log(villanos[index]);
+    }
+
--- src/numeros.ts
+++ src/numeros.ts
@@ -1,12 +1,12 @@
 let avenger:number=5;
-let villanos:number;
+//let villanos:number;
 let otros=2;
 
-if(avenger>villanos){ //error por undefine se debe inicializar
+if(avenger/*>villanos*/){ //error por undefine se debe inicializar
     console.log("estamos salvados");
 }else{
     console.log("estamos muetos");
 }
 
 otros=3.44;
-otros="cadena";//no acepta otro tipo de datos que no sea númerico
+//otros="cadena";//no acepta otro tipo de datos que no sea númerico
