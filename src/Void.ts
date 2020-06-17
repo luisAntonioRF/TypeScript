9cccdaa9512edd2041704ec53f8502a454253598
anton <ramirezfonseca.la@gmail.com>
2020-06-16 21:28:30 -0500

    new Void.ts

---
+++ src/Void.ts
@@ -0,0 +1,11 @@
+/**
+ * Al igual que en lenguajes como Java, void se utiliza donde no hay ningún tipo de datos. 
+ * Por ejemplo, en el tipo de valor devuelto de funciones que no devuelven ningún valor.
+ */
+
+ function sayHi():void{
+     console.log("Hola TypeScript!!");
+ }
+
+ let saludo:void=sayHi();
+
